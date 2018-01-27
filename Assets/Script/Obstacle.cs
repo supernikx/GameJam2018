213aa2a175040c265c6457b657967d877da26bb9
TamagnaDavide <supernikx@gmail.com>
2018-01-27 10:05:24 +0100

    Aggiunto script ObstacleTriangle che fa ruotare su se stesso l'ostacolo su cui è attaccato, e implementato sprite del triangolo

--- Assets/Script/Obstacle.cs
+++ Assets/Script/Obstacle.cs
@@ -3,17 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-[Serializable]
 public class Obstacle : MonoBehaviour {
-
     public float follingSpeed;
     public float addDelay;
     Rigidbody2D rb2d;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rb2d = GetComponent<Rigidbody2D>();
-        rb2d.velocity = new Vector3(0,-follingSpeed,0);
-	}
+        rb2d.velocity = new Vector3(0, -follingSpeed, 0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
---
+++ Assets/Script/ObstacleTriangle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleTriangle : Obstacle {
+
+    public float rotatingSpeed;
+    Rigidbody2D rb2d;
+    private int rotationdirection;
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector3(0, -follingSpeed, 0);
+        rotationdirection = Random.Range(0,2);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (rotationdirection == 0)
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * rotatingSpeed);
+        }
+        else if (rotationdirection == 1)
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * rotatingSpeed);
+        }
+	}
+}
