e08cf6d1e5864544d5401e98b5b9096586a0eb55
TamagnaDavide <supernikx@gmail.com>
2018-01-27 14:57:30 +0100

    Aggiunto obstacle clicker

--- Assets/Script/ObstacleClicker/ObstacleClicker.cs
+++ Assets/Script/ObstacleClicker/ObstacleClicker.cs
@@ -6,10 +6,13 @@ public class ObstacleClicker : Obstacle {
     public int click;
     public Sprite[] sprites = new Sprite[0];
     private SpriteRenderer sprite;
+    Rigidbody2D rb2d;
     private void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
         sprite.sprite = sprites[0];
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector3(0, -fallingSpeed, 0);
     }
 
     public void ButtonClicked()
--- Assets/Script/ObstacleClicker/ObstacleClickerButton.cs
+++ Assets/Script/ObstacleClicker/ObstacleClickerButton.cs
@@ -3,19 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstacleClickerButton : MonoBehaviour {
-    public ObstacleClicker parent;
+    private ObstacleClicker parent;
     private void Start()
     {
         parent = gameObject.transform.parent.gameObject.GetComponent<ObstacleClicker>();
     }
     private void OnMouseDown()
     {
-        Debug.Log("click");
         parent.ButtonClicked();
-    }
-
-    private void OnMouseEnter()
-    {
-        Debug.Log("dsds");
     }
 }
