4b5557411b694017c5c51b3098e674437b26f97c
MarcoMarchitelli <marchitelli97@libero.it>
2018-01-27 04:02:57 -0800

    Created game manager, background scrolling down

---
+++ Assets/Script/BackgroundBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundBehaviour : MonoBehaviour {
+
+	public float movePerSecond;
+
+	void Update () {
+		transform.position += new Vector3 (transform.position.x, -movePerSecond * Time.deltaTime, 0f);
+	}
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public Transform playerTransform;
+	public GameObject background;
+
+	float backgroundSpawnOffset = 0.1f;
+	GameObject lastInstantiatedBackground;
+
+	void Start(){
+		lastInstantiatedBackground = Instantiate (background, Vector3.zero, Quaternion.identity);
+	}
+
+	void Update(){
+		if (lastInstantiatedBackground.transform.position.y < -Camera.main.orthographicSize) {
+			GameObject instantiatedBackground = Instantiate (background, new Vector3(0f, lastInstantiatedBackground.transform.position.y + lastInstantiatedBackground.GetComponent<SpriteRenderer>().sprite.bounds.size.y - backgroundSpawnOffset, 0f), Quaternion.identity);
+			Destroy (instantiatedBackground, 8f);
+			lastInstantiatedBackground = instantiatedBackground;
+		}
+	}
+}
