83be61c912ef0b16052a02654aa9a6c15bb34396
MarcoMarchitelli <marchitelli97@libero.it>
2018-01-27 15:29:18 +0100

    VersioneIncredibile

--- Assets/Script/ObstacleTypes/ObstacleButton.cs
+++ Assets/Script/ObstacleTypes/ObstacleButton.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleButton : MonoBehaviour {
+public class ObstacleButton : Obstacle {
 
-	public int buttonNumber;
+	Rigidbody2D rb2d;
 
+	public GameObject[] buttonsArray;
+	public GameObject[] fitObstaclesToSpawn;
 
+	[HideInInspector]
+	public int hp;
 
+	float screenWidth;
+
+	void Awake(){
+		rb2d = GetComponent<Rigidbody2D>();
+		rb2d.velocity = new Vector3(0, -this.fallingSpeed, 0);
+		hp = buttonsArray.Length;
+		screenWidth = Camera.main.aspect * Camera.main.orthographicSize - 1f;
+	}
+
+	void Update(){
+		if (hp <= 0)
+			Die ();
+	}
+
+	void Die(){
+		int randomIndex = Random.Range(0, fitObstaclesToSpawn.Length);
+		float randomXSPawnPoint = Random.Range(-screenWidth, screenWidth);
+		Vector3 spawnPoint = new Vector3 (randomXSPawnPoint, transform.position.y, 0f);
+		GameObject instantiadetFitObstacle = Instantiate (fitObstaclesToSpawn[randomIndex], spawnPoint, Quaternion.identity);
+		Destroy (instantiadetFitObstacle, instantiadetFitObstacle.GetComponent<Obstacle>().lifeTime);
+		Destroy (gameObject);
+	}
 }
---
+++ Assets/Script/RedButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedButton : MonoBehaviour {
+
+	public Sprite buttonPressedSprite;
+
+	SpriteRenderer mySpriteRenderer;
+	ObstacleButton parentObstacle;
+
+	void Awake(){
+		mySpriteRenderer = GetComponent<SpriteRenderer> ();
+		parentObstacle = GetComponentInParent<ObstacleButton> ();
+	}
+
+	void OnMouseDown(){
+		parentObstacle.hp--;
+		mySpriteRenderer.sprite = buttonPressedSprite;
+	}
+}
